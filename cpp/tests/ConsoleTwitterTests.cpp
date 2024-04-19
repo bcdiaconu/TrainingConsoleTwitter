6297a290eac52cd6d35c7a24761d311f119b337c
Alex Bolboaca <alexboly@gmail.com>
2024-04-19 10:09:33 +0100

    Refactor tests. Add test for individual timeline.

--- cpp/tests/ConsoleTwitterTests.cpp
+++ cpp/tests/ConsoleTwitterTests.cpp
@@ -2,9 +2,14 @@
 #include "doctest.h"
 #include "ConsoleTwitter.h"
 
-TEST_CASE("Empty wall before post")
-{
-	auto wallContents = sendInput("Alice");
+TEST_CASE("No post for Alice before post"){
+	auto posts = sendInput("Alice");
+
+	CHECK_EQ(0, posts.size());
+}
+
+TEST_CASE("Empty wall for Alice before post"){
+	auto wallContents = sendInput("Alice wall");
 
 	CHECK_EQ(0, wallContents.size());
 }
