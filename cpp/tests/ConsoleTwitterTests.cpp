bbfb76620c93ac48121bfac982fdf672ab121b73
Alex Bolboaca <alexboly@gmail.com>
2024-04-19 20:11:31 +0100

    Only Bob's message on Bob's timeline after both Alice and Bob post

--- cpp/tests/ConsoleTwitterTests.cpp
+++ cpp/tests/ConsoleTwitterTests.cpp
@@ -4,21 +4,21 @@
 
 TEST_CASE("Empty timeline for Alice before post"){
 	ConsoleTwitter consoleTwitter;
-	std::string output = consoleTwitter.sendInput("Alice");
+	string output = consoleTwitter.sendInput("Alice");
 
 	CHECK_EQ("Alice Timeline\nNo posts found\n", output);
 }
 
 TEST_CASE("Empty wall for Alice before post"){
 	ConsoleTwitter consoleTwitter;
-	std::string output = consoleTwitter.sendInput("Alice wall");
+	string output = consoleTwitter.sendInput("Alice wall");
 
 	CHECK_EQ("Alice Wall\nNo posts found\n", output);
 }
 
 TEST_CASE("One post on Alice's timeline after posting"){
 	ConsoleTwitter consoleTwitter;
-	std::string output = consoleTwitter.sendInput("Alice -> A message");
+	string output = consoleTwitter.sendInput("Alice -> A message");
 
 	CHECK_EQ("Alice posted message 'A message'\n", output);
 
@@ -29,9 +29,19 @@ TEST_CASE("One post on Alice's timeline after posting"){
 
 TEST_CASE("Bob's timeline is empty after Alice posts") {
 	ConsoleTwitter consoleTwitter;
-	std::string output = consoleTwitter.sendInput("Alice -> A message");
+	string output = consoleTwitter.sendInput("Alice -> A message");
 
 	output = consoleTwitter.sendInput("Bob");
 
 	CHECK_EQ("Bob Timeline\nNo posts found\n", output);
+}
+
+TEST_CASE("Bob's timeline shows only Bob's post after both Alice and Bob post") {
+	ConsoleTwitter consoleTwitter;
+	consoleTwitter.sendInput("Alice -> Alice's message");
+	consoleTwitter.sendInput("Bob -> Bob's message");
+
+	string output = consoleTwitter.sendInput("Bob");
+
+	CHECK_EQ("Bob Timeline\nBob's message\n", output);
 }
